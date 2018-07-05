f2c068c0fb4e33b38b0cb6063224bb3ecea35a5a
brookeclouston <brooke.clouston@canada.ca>
2018-07-05 18:57:23 +0000

    updating names

--- 6046.py
+++ 6046.py
@@ -9,53 +9,60 @@ from tkinter import ttk
 import tkinter as tk
 import itertools
 
-db = MySQLdb.connect("localhost", 'brookec', 'temppass', 'specify')
+db = MySQLdb.connect("localhost", '''"MySQLusername", "MySQLpassword", "MySQLdatabaseName"''')
 
 fetchRecords = db.cursor()
 headings = ["Catalog Number","DAO Accession Number","Collection","Collector Last Name(s)","Taxon Name","Geography","Year Collected"]
 counter=0
 
 # formats the raw data returned from the database search by filtering out duplicate records, counting records with
-# duplicate catalog numbers and handling records with multiple collectors and/or multiple geography names
+# duplicate catalog numbers and handling records with multiple collectors and/or multiple names
 def format_records(raw_data):
     raw_data_dict = {}
     key_data = []
-    raw_list = []
     for record in raw_data:
+        if record[0] not in key_data: key_data.append(record[0])
         key = (record[0],record[4],record[2])
-        if (key in raw_data_dict) and (record[3] not in raw_data_dict[key][3][0]) and (record[2] == raw_data_dict[key][2][0]):
-            raw_data_dict[key][3][0] +=', ' + str(record[3])
+        if (key in raw_data_dict) and (record[3] not in raw_data_dict[key][3][0]) and (record[2]==raw_data_dict[key][2][0]):
+            raw_data_dict[key][3][0] +=", " + str(record[3])
         else:
             raw_data_dict[key]=[[record[0]],[record[1]],[record[2]],[str(record[3])],[record[4]],[record[5]],[record[6]]]
-    for entry in raw_data_dict:
-        raw_list.append(raw_data_dict[entry])
-        if entry[0] not in key_data: key_data.append(entry[0])
-    return raw_list,len(key_data)
+    return raw_data_dict,len(key_data)
 
-# selects all required information from the database using an identifying statement with the formatted user specified fields
-def fetch_info(identifying_statement):
-    fetchRecords.execute("SELECT CO.CatalogNumber,CO.AltCatalogNumber,CL.CollectionName,A.LastName,T.Fullname,G.FullName,YEAR(CE.StartDate) FROM collectionobject CO "
-                        "INNER JOIN collection CL ON CL.CollectionID=CO.CollectionID "
-                        "INNER JOIN collectingevent CE ON CE.CollectingEventID=CO.CollectingEventID "
-                        "INNER JOIN collector C ON C.CollectingEventID=CE.CollectingEventID "
-                        "INNER JOIN determination D ON D.CollectionObjectID=CO.CollectionObjectID "
-                        "INNER JOIN agent A ON A.AgentID=C.AgentID "
-                        "INNER JOIN taxon T ON T.TaxonID=D.TaxonID "
-                        "INNER JOIN locality L ON L.LocalityID=CE.LocalityID "
-                        "INNER JOIN geography G ON G.GeographyID=L.GeographyID "
-                        "WHERE %s" % identifying_statement)
+# selects all required information from the database using an identifying statement with the formatted
+#  user specified statement
+def fetch_info(statement):
+    fetchRecords.execute("SELECT CO.CatalogNumber,CO.AltCatalogNumber,CL.CollectionName,A.LastName,T.Fullname,"
+                         "G.FullName,YEAR(CE.StartDate) FROM collectionobject CO "
+                         "INNER JOIN collection CL ON CL.CollectionID=CO.CollectionID "
+                         "INNER JOIN collectingevent CE ON CE.CollectingEventID=CO.CollectingEventID "
+                         "INNER JOIN collector C ON C.CollectingEventID=CE.CollectingEventID "
+                         "INNER JOIN determination D ON D.CollectionObjectID=CO.CollectionObjectID "
+                         "INNER JOIN agent A ON A.AgentID=C.AgentID "
+                         "INNER JOIN taxon T ON T.TaxonID=D.TaxonID "
+                         "INNER JOIN locality L ON L.LocalityID=CE.LocalityID "
+                         "INNER JOIN geography G ON G.GeographyID=L.GeographyID "
+                         "WHERE %s" % statement)
+    print('here 5')
     return format_records(fetchRecords.fetchall())
 
-# configures user input into a statement to search database
+# configures conditions into a statement that MySQL is able to interpret
 def return_entry():
-    input_list = [("CO.CatalogNumber LIKE'%s'",catalognum.get()),("CO.AltCatalogNumber LIKE'%s'", dao.get()), ("A.LastName LIKE '%s%s%s'", ('%', lastname.get(), '%')),
-                  ("T.FullName LIKE'%s'",geography.get()),("YEAR(CE.StartDate) LIKE '%s'", year.get()), ("G.FullName LIKE '%s%s%s'", ('%', province.get(), '%'))]
-    statement_list = []
+
+    input_list = [("CO.CatalogNumber LIKE'%s'",catalognum.get()),("CO.AltCatalogNumber LIKE'%s'",dao.get()),
+                  ("A.LastName LIKE '%s%s%s'",('%',lastname.get(),'%')),("T.FullName LIKE'%s'",geography.get()),
+                  ("YEAR(CE.StartDate) LIKE '%s'",year.get()),("G.FullName LIKE '%s%s%s'",('%',province.get(),'%'))]
+    #statement_list = []
     for field in input_list:
-        if field[1] != "": statement_list.append(field[0] % field[1] + "AND ")
+        print(input_list)
+        print(field)
+        statement_list = [(field[0] % field[1] + "AND ") if "" not in field else None]
+
+        #if field[1] != "": statement_list.append(field[0] % field[1] + "AND ")
+    print(("".join(statement_list))[:-4])
     return fetch_info(("".join(statement_list))[:-4])
 
-# saves the query results to a .xls file named by the user
+# saves the search results to a .xls file named by the user
 def save_to_file(records):
     name = (format(askstring("Save Query Search","Save results of query as: ")))
     if name != 'None':
@@ -68,7 +75,7 @@ def save_to_file(records):
         ws.set_remove_splits(True)
         for col1,value in enumerate(headings): ws.write(row1,col1,value,heading_xf)
         for i,row2 in enumerate(records):
-            for j,col2 in enumerate(row2): ws.write(i+1,j,col2[0])
+            for j,col2 in enumerate(records[row2]): ws.write(i+1,j,col2[0])
         wb.save("%s.xls" % name)
 
 # configures the display of the results of each query
@@ -88,19 +95,20 @@ def configure_results(records,tab_frame):
         canvas.create_window((4, 4), window=frame, anchor="nw")
         frame.bind("<Configure>", lambda event, canvas=canvas: canvas.configure(scrollregion=canvas.bbox("all")))
         Label(frame, text="Total Records Found: %s " % len(records[0]), font="bold").grid(column=0,row=0)
-        Label(frame, text= "Total Distinct Catalog Numbers: %s" % records[1], font="bold").grid(column=1,row=0)
+        Label(frame, text="Total Distinct Catalog Numbers: %s" % records[1], font="bold").grid(column=1,row=0)
         Button(frame,text="Save Query",command= lambda: save_to_file(records[0])).grid(column=2,row=0)
         Button(frame,text='Close Tab', command= lambda: tab_frame.destroy()).grid(column=3,row=0)
         return frame
 
-# displays the results of each query to a new tab inside the initial window
+# displays the results of each query to a new tab inside the original window
 def display_results(tab_frame):
     records = return_entry()
-    if len(records[0]) != 0:
+    if records[1] != 0:
         frame = configure_results(records,tab_frame)
         for col, value in enumerate(headings): Label(frame, text="%s" % value, font="bold", relief=RIDGE).grid(column=col,row=1, sticky=NSEW)
         for i, row in enumerate(records[0]):
-            for j, data in enumerate(row): Label(frame, text="%s" % data[0], relief=RIDGE).grid(column=j,row=i+2,sticky=NSEW)
+            for j, data in enumerate(records[0][row]):
+               Label(frame, text="%s" % data[0], relief=RIDGE).grid(column=j,row=i+2,sticky=NSEW)
     else:
         messagebox.showinfo("Specify Collections Search", "No records found")
 
