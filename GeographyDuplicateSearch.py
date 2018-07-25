2f442c3c1a395fb5c444a773eb9bfe7828783db8
brookeclouston <brooke.clouston@canada.ca>
2018-07-25 17:07:31 +0000

    Added __main__, csv file writing

--- GeographyDuplicateSearch.py
+++ GeographyDuplicateSearch.py
@@ -1,72 +1,81 @@
 """
 Redmine Support #12202
-Creates an xls report of duplicate geography names within a country. First creates a tree using geography ranks using
-library 'anytree' then iterates through each country subtree and compares each name with each other, if two geography
-full names are a match then they are considered a duplicate and are added to the report.
+Creates a csv report of duplicate geography names within a country. Creates a tree of geographyID/parentID relationships
+using geography ranks with library 'anytree' then iterates through each country subtree and compares each name,
+duplicate full names are added to the report.
 """
 import pymysql as MySQLdb
 from anytree import Node, PreOrderIter
-import xlwt
+import csv
 import itertools
 
-db = MySQLdb.connect("localhost", '''"MySQLusername", "MySQLpassword", "MySQLdatabaseName"''')
-
-fetchRankIDs = db.cursor()
-recordsFromRank = db.cursor()
-
-fetchRankIDs.execute("SELECT DISTINCT RankID FROM geography WHERE RankID >=200 ORDER BY RankID ASC")
-rankIDs = fetchRankIDs.fetchall()
-
-treeDict = {}
-recordsByRank = {}
-resultData = []
-root = Node("root")
-
-# creates new node and new treeDict key, sets parent to the node that was created before
-def add_node(name, gid, pid, previous_parent):
-    node = Node((name, gid), parent=previous_parent)
-    treeDict[gid] = (name, pid, node)
-    return node
+# selects ranks within the geography schema
+def fetch_ranks(db):
+    db_rank_id = db.cursor()
+    db_rank_id.execute("SELECT DISTINCT RankID FROM geography WHERE RankID >=200 ORDER BY RankID ASC")
+    return db_rank_id.fetchall()
 
 # selects all records with a certain rankID and puts them in a dictionary
-for iD in rankIDs:
-    recordsFromRank.execute("SELECT ParentID, FullName, GeographyID FROM geography WHERE RankID = %s", (iD[0]))
-    recordsByRank[iD[0]] = recordsFromRank.fetchall()
+def rank_dict(db):
+    rank_records_dict = {}
+    db_record_info = db.cursor()
+    for iD in fetch_ranks(db):
+        db_record_info.execute("SELECT ParentID, FullName, GeographyID FROM geography WHERE RankID = %s", (iD[0]))
+        rank_records_dict[iD[0]] = db_record_info.fetchall()
+    return rank_records_dict
 
-# builds the geography tree by searching for a relationship between ParentID's and GeographyID's
-for r in recordsByRank:
-    for record in recordsByRank[r]:
-        if record[0] not in treeDict:
-            a = add_node(record[1], record[2], record[0], root)
-        else:
-            b = treeDict[record[0]][2]
-            newNode = add_node(record[1], record[2], record[0], b)
+# creates new node and new tree key, sets parent to the node that was created before
+def add_node(name, gid, pid, previous_parent, tree):
+    node = Node((name, gid), parent=previous_parent)
+    tree[gid] = (name, pid, node)
+    return node
+
+# builds the geography tree by searching relationships between existing geographyID's and new parentID's within the tree
+def build_tree(rank_records_dict):
+    tree = {}
+    root = Node("root")
+    for r in rank_records_dict:
+        for record in rank_records_dict[r]:
+            if record[0] not in tree:
+                add_node(record[1], record[2], record[0], root, tree)
+            else:
+                b = tree[record[0]][2]
+                add_node(record[1], record[2], record[0], b,tree)
+    return tree
 
 # searches each country 'subtree' for matching names
-for country in recordsByRank[200]:
-    country_dict = {}
-    for name in [node.name for node in PreOrderIter((treeDict[country[2]][2]))]:
-        if name[0] in country_dict: country_dict[name[0]].append(name[1])
-        else: country_dict[name[0]] = [name[1]]
-    for key in country_dict:
-        if len(country_dict[key]) > 1:
-            resultData.append((country[1],key,str(country_dict[key])))
+def search_tree(rank_records_dict,tree):
+    countries = rank_records_dict[200]
+    data = []
+    for country in countries:
+        country_dict = {}
+        for name in [node.name for node in PreOrderIter((tree[country[2]][2]))]:
+            if name[0] in country_dict:
+                country_dict[name[0]].append(name[1])
+            else:
+                country_dict[name[0]] = [name[1]]
+        for key in country_dict:
+            if len(country_dict[key]) > 1:
+                data.append((country[1],key,str(country_dict[key])))
+    return data
 
-# writes the results of the search to an xls file named '12202Report'
-wb = xlwt.Workbook()
-ws = wb.add_sheet("Geography Duplicate Results")
-heading_xf = xlwt.easyxf("font: bold on; align: wrap on, vert centre, horiz center")
-headings = ["Country FullName", "Duplicate FullName", "GeographyIDs"]
-rowx = 0
-ws.set_panes_frozen(True)
-ws.set_horz_split_pos(rowx + 1)
-ws.set_remove_splits(True)
-for colx, value in enumerate(headings):
-    ws.write(rowx, colx, value, heading_xf)
-for i, row in enumerate(resultData):
-    for j, col in enumerate(row):
-        ws.write(i + 1, j, col)
-v = [len(row[0]) for row in resultData]
-ws.col(0).width = 256 * max(v) if v else 0
-wb.save("GeographyDuplicateReport.xls")
-db.close()
+# writes the duplicate data passed in to a csv file named 'GeographyDuplicateReport.csv'
+def write_report(data):
+    with open("GeographyDuplicateReport.csv", "w") as file_writer:
+        writer = csv.writer(file_writer)
+        writer.writerow(["Country Name", "Duplicate Geography Full Name", "Geography IDs"])
+        for row in data:
+            writer.writerow(row)
+    print("Report saved as 'GeographyDuplicateReport.csv'")
+
+# calls on functions
+def main():
+    db = MySQLdb.connect("localhost", '''"MySQLusername", "MySQLpassword", "MySQLdatabaseName"''')
+    print("Searching for duplicate geography full names in the same country ")
+    rank_records_dict = rank_dict(db)
+    tree = build_tree(rank_records_dict)
+    result_data = search_tree(rank_records_dict, tree)
+    write_report(result_data)
+
+if __name__ == "__main__":
+    main()
--- GeographyTypoSearch.py
+++ GeographyTypoSearch.py
@@ -1,71 +1,79 @@
 """
 Redmine Support #12203
-Creates an xls report of possible typos within the geography tree. First builds a tree by rank using the library
-'anytree' and excludes any geography full names with numbers in them to avoid any trivial typos (ie. 'Zone 1' vs 'Zone 2')
-Then searches through each country subtree, if two names have a levenshtein distance of 1 then they are flagged as
-possible typos and are added to the report.
+Creates a csv report of possible typos in geography full names that are in the same country. Creates a geography tree
+of parentID/taxonID relationships using the library 'anytree', then iterates through each country subtree and takes the
+levenshtein distance between two names. Names with a LD = 1 are flagged as possible typos and added to the report.
 """
 import pymysql as MySQLdb
 from anytree import Node, PreOrderIter
-import xlwt
+import csv
 import itertools
 from Levenshtein import distance
 
-db = MySQLdb.connect("localhost", '''"MySQLusername", "MySQLpassword", "MySQLdatabaseName"''')
-
-fetchRankIDs = db.cursor()
-recordsFromRank = db.cursor()
-
-fetchRankIDs.execute("SELECT DISTINCT RankID FROM geography WHERE RankID >=200 ORDER BY RankID ASC")
-rankIDs = fetchRankIDs.fetchall()
-
-treeDict = {}
-recordsByRank = {}
-resultData = []
-root = Node("root")
-
-# creates new node and new treeDict key, sets parent to the node that was created before
-def add_node(name, gid, pid,previous_parent):
-    node = Node((name, gid), parent=previous_parent)
-    treeDict[gid] = (name, pid, node)
-    return node
+# selects ranks within the geography schema
+def fetch_ranks(db):
+    db_rank_id = db.cursor()
+    db_rank_id.execute("SELECT DISTINCT RankID FROM geography WHERE RankID >=200 ORDER BY RankID ASC")
+    return db_rank_id.fetchall()
 
 # selects all records with a certain rankID and puts them in a dictionary
-for iD in rankIDs:
-    recordsFromRank.execute("SELECT ParentID, FullName, GeographyID FROM geography WHERE RankID = %s ", (iD[0]))
-    recordsByRank[iD[0]] = recordsFromRank.fetchall()
+def rank_dict(db):
+    rank_records_dict = {}
+    db_record_info = db.cursor()
+    for iD in fetch_ranks(db):
+        db_record_info.execute("SELECT ParentID, FullName, GeographyID FROM geography WHERE RankID = %s", (iD[0]))
+        rank_records_dict[iD[0]] = db_record_info.fetchall()
+    return rank_records_dict
 
-# builds the geography tree by searching for a relationship between PIDs and GIDs and excludes names with numbers
-for r in recordsByRank:
-    for record in recordsByRank[r]:
-        if (any(str.isdigit(c) for c in record[1])) is False:
-            if record[0] not in treeDict:
-                a = add_node(record[1], record[2], record[0], root)
-            else:
-                b = treeDict[record[0]][2]
-                newNode = add_node(record[1], record[2], record[0], b)
+# creates new node and new tree key, sets parent to the node that was created before
+def add_node(name, gid, pid, previous_parent, tree):
+    node = Node((name, gid), parent=previous_parent)
+    tree[gid] = (name, pid, node)
+    return node
+
+# builds the geography tree by searching relationships between existing geographyID's and new parentID's within the tree
+def build_tree(rank_records_dict):
+    tree = {}
+    root = Node("root")
+    for r in rank_records_dict:
+        for record in rank_records_dict[r]:
+            if (any(str.isdigit(c) for c in record[1])) is False:
+                if record[0] not in tree:
+                    add_node(record[1], record[2], record[0], root, tree)
+                else:
+                    b = tree[record[0]][2]
+                    add_node(record[1], record[2], record[0], b,tree)
+    return tree
 
 # searches each country 'subtree' and compares names, searching for names with a levenshtein distance of 1
-for country in recordsByRank[200]:
-    for name1, name2 in itertools.combinations(([node.name for node in PreOrderIter((treeDict[country[2]][2]))]), 2):
-        LD = distance(name1[0], name2[0])
-        if LD == 1:
-            resultData.append((country[1], name1[0], name2[0], name1[1], name2[1], LD))
+def search_tree(rank_records_dict,tree):
+    data=[]
+    countries = rank_records_dict[200]
+    for country in countries:
+        for name1, name2 in itertools.combinations(([node.name for node in PreOrderIter((tree[country[2]][2]))]), 2):
+            ld = distance(name1[0], name2[0])
+            if ld == 1:
+                print((country[1], name1[0], name2[0], name1[1], name2[1], ld))
+                data.append((country[1], name1[0], name2[0], name1[1], name2[1], ld))
+    return data
 
-# writes the results of the search to an xls file named 'Geography Typo Report'
-wb = xlwt.Workbook()
-ws = wb.add_sheet("Geography Typo Report",cell_overwrite_ok= True)
-heading_xf = xlwt.easyxf("font: bold on; align: wrap on, vert centre, horiz center")
-headings = ["Country", "FullName 1","FullName 2","GeographyID 1", "GeographyID 2", "Levenshtein Distance"]
-rowx = 0
-ws.set_panes_frozen(True)
-ws.set_horz_split_pos(rowx+1)
-ws.set_remove_splits(True)
-for colx, value in enumerate(headings):
-    ws.write(rowx, colx, value, heading_xf)
-for i, row in enumerate(resultData):
-    for j, col in enumerate(row):
-        ws.write(i+1, j, col)
-ws.col(0).width = 256 * max([len(row[0]) for row in resultData])
-wb.save("GeographyTypoReport.xls")
-db.close()
+# writes the duplicate data passed in to a csv file named 'GeographyTypoReport.csv'
+def write_report(data):
+    with open("GeographyTypoReport.csv", "w") as file_writer:
+        writer = csv.writer(file_writer)
+        writer.writerow(["Country", "FullName 1","FullName 2","GeographyID 1", "GeographyID 2", "Levenshtein Distance"])
+        for row in data:
+            writer.writerow(row)
+    print("Report saved as 'GeographyTypoReport.csv'")
+
+# calls on functions
+def main():
+    db = MySQLdb.connect("localhost", '''"MySQLusername", "MySQLpassword", "MySQLdatabaseName"''')
+    print("Searching for possible typos in geography full names within the same country")
+    rank_records_dict = rank_dict(db)
+    tree = build_tree(rank_records_dict)
+    result_data = search_tree(rank_records_dict, tree)
+    write_report(result_data)
+
+if __name__ == "__main__":
+    main()
