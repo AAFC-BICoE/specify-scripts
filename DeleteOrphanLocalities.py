32e6301710073e926db99195e6b2f78efd52bcf5
brookeclouston <brooke.clouston@canada.ca>
2018-07-30 14:48:09 +0000

    Adding in default report writing, adding more commandline documentaion

--- DeleteOrphanLocalities.py
+++ DeleteOrphanLocalities.py
@@ -44,21 +44,20 @@ def main():
     parser.add_argument("-d", "--database", action="store", dest="database", help="Name of MySQL specify database", required= True)
     parser.add_argument("--show", action="store_true", dest="show", help="Print Locality ID's to be deleted to screen")
     parser.add_argument("--delete", action="store_true", dest="delete", help="Delete orphan localities")
+    parser.add_argument("--report", action="store_true", default = True, dest="report", help="(default) Create a report of what localities will be deleted")
     args = parser.parse_args()
     username = args.username
     password = args.password
     database = args.database
     show= args.show
     delete = args.delete
+    report = args.report
     try:
         db = pymysql.connect("localhost", username, password, database)
     except pymysql.err.OperationalError:
         print('Error connecting to database, try again')
         return
     orphans = orphan_ids(db)
-    # writes a csv file containing the locality ID's passed in
-    file_name = ("LocalitiesDeleted[%s]" % (datetime.date.today()))
-    write_report(file_name, ["Locality ID"], orphans)
     if show:
         for iD in orphans:
             print(iD)
@@ -67,7 +66,13 @@ def main():
             delete_orphans(db,orphans)
     except pymysql.err.DatabaseError:
         print("Error when trying to delete orphan localities")
-    print("Report saved as '%s.csv'" % file_name)
+    if report:
+        file_name = ("LocalitiesDeleted[%s]" % (datetime.date.today()))
+        write_report(file_name, ["Locality ID"], orphans)
+        if not delete:
+            print("No changes made, report of orphan localities saved as %s" % file_name)
+            return print("See -h for more options")
+        return print("Report saved as '%s.csv'" % file_name)
 
 if __name__ == "__main__":
     main()
