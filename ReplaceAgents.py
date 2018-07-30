86bab6d25025ea6c0aaea8168bd4606580155ad1
brookeclouston <brooke.clouston@canada.ca>
2018-07-30 11:58:21 +0000

    Adding support for command line arguments

--- ReplaceAgents.py
+++ ReplaceAgents.py
@@ -2,60 +2,83 @@
 Redmine Support #5722
 Reading from a csv file containing two GUID's, the specified 'Bad Agent' is deleted from the schema by changing all
 references of the 'Bad Agent' to the specified 'Good Agent' using the database schema to look for references of 'AgentID'
-in column names and foreign keys. File path of csv needs to be specified first.
+in column names and foreign keys. Defaults to saving a csv file of what agentIDs will be deleted/updated, will only
+actually preform the changes if '--replace' is added to the command line argument.
 NOTE: csv format should have the GUID of the agent to be deleted FIRST, then the GUID of the agent that the info will be
 changed to SECOND in the same row
 """
-import pymysql as MySQLdb
+import pymysql
+import argparse
 import csv
+import datetime
+from csvwriter import write_report
 
 # selects agentIDs corresponding to provided GUID's
-def agent_info(fetch_id,guid):
-    fetch_id.execute("SELECT AgentID FROM agent WHERE GUID = '%s' " % guid)
-    return fetch_id.fetchall()[0][0]
+def agent_info(db,guid):
+    db_agent_id= db.cursor()
+    db_agent_id.execute("SELECT AgentID FROM agent WHERE GUID = '%s' " % guid)
+    return db_agent_id.fetchall()[0][0]
 
 # updates instances of the 'bad agent' with the good agent then deletes the bad agent
-def update_agents(guid_list,fetch_id,agent_references,update,delete):
+def update_agents(guid_list,agent_references,db,show,replace):
+    db_update = db.cursor()
+    db_delete = db.cursor()
+    agent_list = []
     for agent in guid_list:
         try:
-            bad_agent = agent_info(fetch_id,agent[0])
-            good_agent = agent_info(fetch_id,agent[1])
-            update_agent = input("Update AgentID %s to AgentID %s? [y/n]" % (bad_agent,good_agent))
-            if update_agent == "y":
+            bad_agent = agent_info(db,agent[0])
+            good_agent = agent_info(db,agent[1])
+            agent_list += [(str(bad_agent),str(good_agent))]
+            if show:
+                print('Updating all instances of AgentID %s to AgentID %s' %(bad_agent,good_agent))
+            if replace:
                 for column_name in agent_references:
-                    update.execute("UPDATE %s SET %s = %s WHERE %s = %s"
+                    db_update.execute("UPDATE %s SET %s = %s WHERE %s = %s"
                                    % (column_name[0],column_name[1],good_agent,column_name[1],bad_agent))
-                delete_agent = input("Delete AgentID %s? [y/n]" %bad_agent)
-                if delete_agent == "y":
-                    delete.execute("DELETE FROM agent WHERE AgentID = %s" % bad_agent)
-                    db.commit()
-                elif delete_agent == "n":
-                    pass
-                else:
-                    print("Invalid command")
-            elif update_agent == "n":
-                pass
-            else:
-                print("Invalid command")
+                db_delete.execute("DELETE FROM agent WHERE AgentID = %s" % bad_agent)
+                db.commit()
+                print("Agent replaced")
         except IndexError:
            print("Agent GUID set: ",agent," not found in database" )
+    return agent_list
 
-# creates cursors and list of data from csv file, selects tables with columns and foreign keys that reference agentIDs
+# creates command line arguments, reads GUID data from csv file, selects schema information that references agentIDs
 def main():
-    db = MySQLdb.connect("localhost", '''"MySQLusername", "MySQLpassword", "MySQLdatabaseName"''')
-    fetch_keys = db.cursor()
-    fetch_references = db.cursor()
-    fetch_id = db.cursor()
-    update = db.cursor()
-    delete = db.cursor()
-    with open(".csv", newline='') as csvfile: # Input file path of csv
-        guid_list = [row[0].split(",") for row in csv.reader(csvfile,delimiter=" ", quotechar="|")]
-    fetch_keys.execute("SELECT TABLE_NAME, COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE "
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username", action="store", dest="username", help="MySQL username", required=True)
+    parser.add_argument("-p", "--password", action="store", dest="password", help="MySQL password", required= True)
+    parser.add_argument("-d", "--database", action="store", dest="database", help="MySQL specify database name", required=True)
+    parser.add_argument("-csv", action="store", dest="file_name", help ="Name of .csv file containing agent GUIDs", required= True)
+    parser.add_argument("--show", action="store_true", dest ="show", help="Print the agentID's to screen")
+    parser.add_argument("--replace", action="store_true",dest="replace",help="Replace agents")
+    args = parser.parse_args()
+    username = args.username
+    password = args.password
+    database = args.database
+    file_name = args.file_name
+    show = args.show
+    replace = args.replace
+    try:
+        db = pymysql.connect("localhost", username, password, database)
+    except pymysql.err.OperationalError:
+        return print('Error connecting to database, try again')
+    db_foreign_keys = db.cursor()
+    db_references = db.cursor()
+    try:
+        with open("%s"% file_name, newline='')  as csvfile: # Input file path of csv
+            guid_list = [row[0].split(",") for row in csv.reader(csvfile,delimiter=" ", quotechar="|")]
+    except FileNotFoundError:
+        return print("%s not found" % file_name)
+    db_foreign_keys.execute("SELECT TABLE_NAME, COLUMN_NAME FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE "
                        "WHERE CONSTRAINT_SCHEMA = 'specify' AND REFERENCED_COLUMN_NAME LIKE '%AgentID%'")
-    fetch_references.execute("SELECT TABLE_NAME, COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE "
+    db_references.execute("SELECT TABLE_NAME, COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE "
                              "COLUMN_NAME LIKE '%AgentID%' AND TABLE_NAME != 'AGENT'")
-    agent_references = (fetch_keys.fetchall() + fetch_references.fetchall())
-    update_agents(guid_list,fetch_id,agent_references,update,delete)
+    agent_references = (db_foreign_keys.fetchall() + db_references.fetchall())
+    agent_list = update_agents(guid_list,agent_references,db,show,replace)
+    name = "AgentsReplaced[%s]" % (datetime.date.today())
+    heading =["Removed AgentID","AgentID"]
+    write_report(name,heading,agent_list)
+    print("Report saved as %s.csv" % name)
 
 if __name__ == "__main__":
     main()
