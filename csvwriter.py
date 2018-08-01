a524587583bdf2609026dd533b3e770c9ac22c2e
brookeclouston <brooke.clouston@canada.ca>
2018-08-01 17:58:14 +0000

    adding compatibility for cross platform csv file writing

--- csvwriter.py
+++ csvwriter.py
@@ -1,11 +1,14 @@
 """
-writes data passed in to a csv file with heading
+writes data passed in to a csv file with heading, compatible with linux and windows
 """
-import csv
+import csv, sys
 
 def write_report(file_name,heading,data):
-    with open("%s.csv" % file_name, "w") as file_writer:
-        writer = csv.writer(file_writer)
-        writer.writerow(heading)
-        for row in data:
-            writer.writerow(row)
+    if sys.version_info >= (3, 0, 0):
+        file_writer = open("%s.csv" % file_name, 'w', newline='')
+    else:
+        file_writer = open("%s.csv" % file_name, 'wb')
+    writer = csv.writer(file_writer)
+    writer.writerow(heading)
+    for row in data:
+        writer.writerow(row)
