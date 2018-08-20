4e8cc006825474946315f754070a7aa30c20c293
brookeclouston <brooke.clouston@canada.ca>
2018-08-20 19:23:40 +0000

    Added in pylint suggestions, rename

---
+++ MultiCollectionQuery.py
@@ -0,0 +1,123 @@
+"""
+Searches across all collections within the schema by restrictions set from command line arguments.
+Using a MySQL statement that joins the required tables and a formatted list of the restrictions,
+the CatalogNumber, DAO Accession Number, Collection Name, Collector Last Name, Taxon Full Name,
+Geography and Year Collected of records satisfying the restraints are returned. A report is then
+created with this data.
+"""
+import argparse
+import datetime
+import pymysql
+from csvwriter import write_report
+
+def format_records(query_data):
+    # Formats records with multiple collectors, returns the number of distinct catalog numbers
+    raw_data = {}
+    catalog_nums = []
+    for record in query_data:
+        if record[0] not in catalog_nums:
+            catalog_nums.append(record[0])
+        key = (record[0], record[4], record[2])
+        if (key in raw_data) and \
+                (record[3] not in raw_data[key][3][0]) and \
+                (record[2] == raw_data[key][2][0]):
+            raw_data[key][3][0] += ", " + str(record[3])
+        else:
+            raw_data[key] = [[record[0]], [record[1]], [record[2]], [str(record[3])],
+                             [record[4]], [record[5]], [record[6]]]
+    clean_data = [[y for x in raw_data[value] for y in x] for value in raw_data]
+    return clean_data, len(catalog_nums)
+
+def fetch_info(database, restriction_list):
+    # Selects required columns from schema using passed in formatted restrictions on data
+    db_fetch_records = database.cursor()
+    db_fetch_records.execute("SELECT CO.CatalogNumber, CO.AltCatalogNumber, CL.CollectionName, "
+                             "A.LastName, T.Fullname, G.FullName, CE.StartDate "
+                             "FROM collectionobject CO "
+                             "INNER JOIN collection CL ON CL.CollectionID = CO.CollectionID "
+                             "INNER JOIN collectingevent CE "
+                             "ON CE.CollectingEventID = CO.CollectingEventID "
+                             "INNER JOIN collector C "
+                             "ON C.CollectingEventID = CE.CollectingEventID "
+                             "INNER JOIN determination D "
+                             "ON D.CollectionObjectID = CO.CollectionObjectID "
+                             "INNER JOIN agent A ON A.AgentID = C.AgentID "
+                             "INNER JOIN taxon T ON T.TaxonID = D.TaxonID "
+                             "INNER JOIN locality L ON L.LocalityID = CE.LocalityID "
+                             "INNER JOIN geography G ON G.GeographyID = L.GeographyID "
+                             "WHERE %s" % restriction_list)
+    return format_records(db_fetch_records.fetchall())
+
+def format_args(catalognum, dao, lastname, taxon, year, province):
+    # Configures data restrictions into a statement that MySQL is able to interpret
+    input_list = [("CO.CatalogNumber LIKE '%s' ", catalognum),
+                  ("CO.AltCatalogNumber LIKE '%s' ", dao),
+                  ("A.LastName LIKE '%s%s%s' ", ('%', lastname, '%')),
+                  ("T.FullName LIKE '%s' ", taxon),
+                  ("YEAR(CE.StartDate) LIKE '%s' ", year),
+                  ("G.FullName LIKE '%s%s%s' ", ('%', province, '%'))]
+    statement_list = list((field[0] % field[1] + "AND ") for field in input_list if field[1] != "")
+    formatted_list = (("".join(statement_list))[:-4])
+    return formatted_list
+
+def save_to_file(data_list, show):
+    # Saves the results of the query to a csv file, prints data to screen if specified
+    file_name = "QueryReport[%s]" % (datetime.date.today())
+    headings = ["Catalog Number", "DAO Accession Number", "Collection", "Collector Last Name(s)",
+                "Taxon Name", "Geography", "Date Collected"]
+    write_report(file_name, headings, data_list[0])
+    if show:
+        for row in data_list[0]:
+            print(*row, sep=", ")
+    print("%s records found | %s distinct catalog numbers" % (len(data_list[0]), data_list[1]))
+    print("Report saved as %s.csv" % file_name)
+
+# Creates command line arguments and coordinates function calling
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username", action="store", dest="username",
+                        help="MySQL username", required=True)
+    parser.add_argument("-p", "--password", action="store", dest="password",
+                        help="MySQL password", required=True)
+    parser.add_argument("-d", "--database", action="store", dest="database",
+                        help="Name of MySQL Specify database", required=True)
+    parser.add_argument("--report", action="store_true", dest="report", default=True,
+                        help="(default) Create a report of query results")
+    parser.add_argument("--show", action="store_true", dest="show", help="Display query results")
+    parser.add_argument("--catalognumber", action="store", dest="catnum", default="",
+                        help="Search collections by CatalogNumber")
+    parser.add_argument("--DAOaccessionnumber", action="store", dest="dao", default="",
+                        help="Search collections by DAO Accession Number (AltCatalogNumber)")
+    parser.add_argument("--lastname", action="store", dest="lastname", default="",
+                        help="Search collections by collector last name")
+    parser.add_argument("--taxon", action="store", dest="taxon", default="",
+                        help="Search collections by taxon full name")
+    parser.add_argument("--year", action="store", dest="year", default="",
+                        help="Search collections by year collected")
+    parser.add_argument("--province", action="store", dest="province", default="",
+                        help="Search collections by province/state object was collected in")
+    args = parser.parse_args()
+    report = args.report
+    show = args.show
+    catnum = args.catnum
+    dao = args.dao
+    lastname = args.lastname
+    taxon = args.taxon
+    year = args.year
+    province = args.province
+    try:
+        database = pymysql.connect("localhost", args.username, args.password, args.database)
+    except pymysql.err.OperationalError:
+        return print("Error connecting to database")
+    if not (catnum or dao or lastname or taxon or year or province):
+        return print("Must enter at least one argument, see -h for more options")
+    if report:
+        format_list = format_args(catnum, dao, lastname, taxon, year, province)
+        data_list = fetch_info(database, format_list)
+        if data_list:
+            return print("No records found")
+        return save_to_file(data_list, show)
+    return None
+
+if __name__ == "__main__":
+    main()
---
+++ test_MultiCollectionQuery.py
@@ -0,0 +1,135 @@
+"""
+8 test methods for the MultiCollectionQuery.py script.
+"""
+import os
+import sqlite3
+import unittest
+import MultiCollectionQuery
+
+class TestDatabase(unittest.TestCase):
+    # Testing the script MultiCollectionQuery.py
+
+    def setUp(self):
+        # Creates the test database and test tables, inserts sample data into test tables
+        conn = sqlite3.connect("specifytest.db")
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE collectionobject (CatalogNumber, CollectionID, "
+                       "CollectingEventID, CollectionObjectID, AltCatalogNumber)")
+        test_collection_obj = [("1", "4", "10", "16", "test1"), ("2", "5", "11", "17", "test2"),
+                               ("3", "6", "12", "18", "test3")]
+        cursor.executemany("INSERT INTO collectionobject VALUES (?,?,?,?,?)", test_collection_obj)
+
+        cursor.execute("CREATE TABLE collection (CollectionID, CollectionName)")
+        test_collection = [("4", "test4"), ("5", "test5"), ("6", "test6")]
+        cursor.executemany("INSERT INTO collection VALUES (?,?)", test_collection)
+
+        cursor.execute("CREATE TABLE collectingevent "
+                       "(CollectingEventID, StartDate DATE, LocalityID)")
+        test_collecting_event = [("10", "2011-01-03", "25"), ("11", "2001-09-08", "26"),
+                                 ("12", "2003-03-12", "27")]
+        cursor.executemany("INSERT INTO collectingevent VALUES (?,?,?)", test_collecting_event)
+
+        cursor.execute("CREATE TABLE collector (CollectingEventID, data1, AgentID )")
+        test_collector = [("10", "13", "19"), ("11", "14", "20"), ("12", "15", "21")]
+        cursor.executemany("INSERT INTO collector VALUES (?,?,?)", test_collector)
+
+        cursor.execute("CREATE TABLE determination (CollectionObjectID, data1, TaxonID)")
+        test_determination = [("16", "7", "22"), ("17", "17", "23"), ("18", "18", "24")]
+        cursor.executemany("INSERT INTO determination VALUES (?,?,?)", test_determination)
+
+        cursor.execute("CREATE TABLE agent (AgentID, LastName)")
+        test_agent = [("19", "test7"), ("20", "test8"), ("21", "test9")]
+        cursor.executemany("INSERT INTO agent VALUES (?,?)", test_agent)
+
+        cursor.execute("CREATE TABLE taxon (TaxonID, FullName)")
+        test_taxon = [("22", "test10"), ("23", "test11"), ("24", "test12")]
+        cursor.executemany("INSERT INTO taxon VALUES (?,?)", test_taxon)
+
+        cursor.execute("CREATE TABLE locality (LocalityID, data1, GeographyID)")
+        test_locality = [("25", "7", "28"), ("26", "8", "29"), ("27", "9", "30")]
+        cursor.executemany("INSERT INTO locality VALUES (?,?,?)", test_locality)
+
+        cursor.execute("CREATE TABLE geography (GeographyID, FullName)")
+        test_geography = [("28", "test13"), ("29", "test14"), ("30", "test15")]
+        cursor.executemany("INSERT INTO geography VALUES (?,?)", test_geography)
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def tearDown(self):
+        # Removes the test database
+        os.remove("specifytest.db")
+
+    def test_format_records_multiple_collectors(self):
+        # Confirms the function returns the expected list when multiple collectors are passed in
+        test_query_data = (("catnum", "dao", "collection", "test1", "taxon", "locality", "year"),
+                           ("catnum", "dao", "collection", "test2", "taxon", "locality", "year"))
+        actual = MultiCollectionQuery.format_records(test_query_data)
+        expected = ([["catnum", "dao", "collection", "test1, test2", "taxon", "locality", "year"]]
+                    , 1)
+        self.assertTupleEqual(expected, actual)
+
+    def test_format_records_multiple_catalog_nums(self):
+        # Confirms the function returns the correct number of unique catalognumbers
+        test_query_data = (("catnum1", "dao", "collection", "test1", "taxon", "locality", "year"),
+                           ("catnum2", "dao", "collection", "test2", "taxon", "locality", "year"))
+        actual = MultiCollectionQuery.format_records(test_query_data)
+        expected = ([["catnum1", "dao", "collection", "test1", "taxon", "locality", "year"],
+                     ["catnum2", "dao", "collection", "test2", "taxon", "locality", "year"]], 2)
+        self.assertEqual(expected, actual)
+
+    def test_fetch_info_restriction_single_positive(self):
+        # Confirms the expected list is returned containing the information from a restriction list
+        conn = sqlite3.connect("specifytest.db")
+        test_restriction_list = "CL.CollectionName LIKE 'test4'"
+        actual = MultiCollectionQuery.fetch_info(conn, test_restriction_list)
+        expected = ([["1", "test1", "test4", "test7", "test10", "test13", "2011-01-03"]], 1)
+        self.assertEqual(expected, actual)
+
+    def test_fetch_info_restriction_single_negative(self):
+        # Confirms an empty list is returned when an invalid restriction is passed in
+        conn = sqlite3.connect("specifytest.db")
+        test_restriction_list = "G.FullName LIKE '1'"
+        actual = MultiCollectionQuery.fetch_info(conn, test_restriction_list)
+        expected = ([], 0)
+        self.assertEqual(expected, actual)
+
+    def test_fetch_info_restriction_multi_positive(self):
+        # Confirms the expected list is returned when multiple valid restrictions are passed in
+        conn = sqlite3.connect("specifytest.db")
+        test_restriction_list = "CO.CatalogNumber LIKE '1' AND A.LastName LIKE 'test7'"
+        actual = MultiCollectionQuery.fetch_info(conn, test_restriction_list)
+        expected = ([["1", "test1", "test4", "test7", "test10", "test13", "2011-01-03"]], 1)
+        self.assertEqual(expected, actual)
+
+    def test_fetch_info_restriction_multi_negative(self):
+        # Confirms an empty list is returned when multiple invalid restrictions are passed in
+        conn = sqlite3.connect("specifytest.db")
+        test_restriction_list = "CO.AltCatalogNumber LIKE '7' AND T.FullName LIKE 'test7'"
+        actual = MultiCollectionQuery.fetch_info(conn, test_restriction_list)
+        expected = ([], 0)
+        self.assertEqual(expected, actual)
+
+    def test_format_args_single(self):
+        # Confirms the expected list is returned when a valid single restriction is passed in
+        test_catalognum = "test"
+        test_dao, test_lastname, test_taxon, test_year, test_province = "", "", "", "", ""
+        actual = MultiCollectionQuery.format_args(test_catalognum, test_dao, test_lastname,
+                                                  test_taxon, test_year, test_province)
+        expected = "CO.CatalogNumber LIKE 'test' AND A.LastName LIKE '%%' AND G.FullName LIKE '%%' "
+        self.assertEqual(expected, actual)
+
+    def test_format_args_all(self):
+        # Confirms the expected list is returned when all possible restrictions are passed in
+        test_catalognum, test_taxon, test_lastname, test_dao, test_year, test_province = \
+            "test1", "test2", "test3", "test4", "test5", "test6"
+        actual = MultiCollectionQuery.format_args\
+            (test_catalognum, test_dao, test_lastname, test_taxon, test_year, test_province)
+        expected = "CO.CatalogNumber LIKE 'test1' AND CO.AltCatalogNumber LIKE 'test4' " \
+                   "AND A.LastName LIKE '%test3%' AND T.FullName LIKE 'test2' " \
+                   "AND YEAR(CE.StartDate) LIKE 'test5' AND G.FullName LIKE '%test6%' "
+        self.assertEqual(expected, actual)
+
+if __name__ == "__main__":
+    unittest.main()
